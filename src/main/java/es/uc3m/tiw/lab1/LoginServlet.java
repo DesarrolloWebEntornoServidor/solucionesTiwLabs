63bb84559e4eb0607a03c4cc2bed3bb5dbe0635e
Tecnologias de Internet para la web <tiw@vagrant.vm>
2018-05-28 19:40:44 +0000

    Taglibs, Lenguaje de Expresiones y dominios

--- src/main/java/es/uc3m/tiw/lab1/LoginServlet.java
+++ src/main/java/es/uc3m/tiw/lab1/LoginServlet.java
@@ -2,7 +2,9 @@ package es.uc3m.tiw.lab1;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +12,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import es.uc3m.tiw.lab1.dominios.Usuario;
+
 import javax.servlet.ServletConfig;
 
 /**
@@ -24,18 +29,20 @@ public class LoginServlet extends HttpServlet {
 	private ServletConfig config;
 	private List<String> listausuarios;
 
+	private Usuario usuario;
+	private ArrayList<Usuario> usuarios;
 
 	@Override
 	public void init(ServletConfig config) throws ServletException {
-	    super.init(config);
+		super.init(config);
 	    this.config = config;
-	    listausuarios = new ArrayList<>();
-		listausuarios.add("carlos");
-		listausuarios.add("eva");
-		listausuarios.add("marta");
-		listausuarios.add("oscar");
+		usuarios = new ArrayList<Usuario>();
+		usuarios.add(new Usuario("Carlos", "Tessier", "carlos", "1234"));
+		usuarios.add(new Usuario("Maria", "perez", "maria", "12345678"));
+		usuarios.add(new Usuario("Daniel", "Garcia", "dani", "12345678"));
+
 	}
-	
+
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
@@ -59,35 +66,56 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		HttpSession sesion = request.getSession();
+		String user = request.getParameter("login").toLowerCase();
+		String password = request.getParameter("password");
+		String mensaje = "";
 		String pagina = LOGIN_JSP;
-		boolean autenticado;
-		
-		if (sesion.getAttribute("autenticado") != null) {
-			autenticado = (boolean) sesion.getAttribute("autenticado");
-		}
-		else{
-			autenticado = false;	
-		}
-		
-		String nombre = request.getParameter("nombre").toLowerCase();
-		String password = request.getParameter("clave");
 
-		if (listausuarios.contains(nombre) && password.equals("1234")) {
+		HttpSession sesion = request.getSession();
+		Usuario u = comprobarUsuario(user, password);
+		
+		Map<String, String> errores = new HashMap<String, String>();
 
-			pagina = LISTADO_JSP;
-			request.setAttribute("usuarios", listausuarios);
+		
+		if (user.equals("")|| password.equals("")) {
 			
+			if (user.equals("")) {
+				errores.put("login", "El user no puede quedar en blanco");	
+			}
+			if (password.equals("")) {
+				errores.put("password", "El campo password no puede estar vacío");
+					
+			}
+			
+			request.setAttribute("errores", errores);
+		}
+		else if (!u.getNombre().equals("")) {
+			pagina = LISTADO_JSP;
+			request.setAttribute("usuarios", usuarios);
+			sesion.setAttribute("usuario", u);
+			sesion.setAttribute("autenticado", true);
 
 		} else {
-			pagina = ERROR_JSP;
+
+			errores.put("loginFailed","Usuario o password incorrectos");
+			request.setAttribute("errores", errores);
+			//pagina = ERROR_JSP;
+			sesion.setAttribute("autenticado",false);
 
 		}
-		if (autenticado) {
-			pagina = LISTADO_JSP;
-					
-		}
-		config.getServletContext().getRequestDispatcher(pagina).forward(request, response);
 
+		this.getServletContext().getRequestDispatcher(pagina).forward(request, response);
+
+	}
+
+	private Usuario comprobarUsuario(String user, String password) {
+		Usuario u = new Usuario();
+		for (Usuario usuario : usuarios) {
+			if (user.equals(usuario.getUsuario()) && password.equals(usuario.getPassword())) {
+				u = usuario;
+				break;
+			}
+		}
+		return u;
 	}
 }
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -1,24 +1,49 @@
+<%@page import="java.util.HashMap"%>
+<%@page import="java.util.Map"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-		<title>Insert title here</title>
-	</head>
-	<body>
-		<h1>Formulario</h1>
-		<form action="login" method="post">
-			<fieldset>
-				<legend>Formulario de login</legend>
-				<label for="nom">Nombre:</label> 
-				<input type="text" name="nombre" id="nom"><br/> 
-				<label for="clave">Password:</label> 
-				<input type="password" name="clave" id="clave"><br/>
-	
-			</fieldset>
-			<input type="submit" value="Enviar">
-	
-		</form>
-	</body>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+<style>
+	.error { color:red;}
+</style>
+</head>
+<body>
+	<!-- comprobar y pintar errores -->
+
+	<c:if test="${!empty errores }">
+		<c:forEach items="${errores }" var="error">
+
+			<c:if test="${error.key =='login'}">
+				<p class="error">El nombre no puede quedar en blanco</p>
+
+			</c:if>
+			<c:if test="${error.key =='password'}">
+				<p class="error">La clave no puede quedar en blanco</p>
+
+			</c:if>
+			<c:if test="${error.key =='loginFailed'}">
+				<p class="error">Usuario y/o contraseña invalidos</p>
+
+			</c:if>
+		</c:forEach>
+	</c:if>
+	<h1>Formulario</h1>
+	<form action="login" method="post">
+		<fieldset>
+			<legend>Formulario de login</legend>
+			<label for="nom">Nombre:</label> 
+			<input type="text" name="login" id="nom"><br/> 
+			<label for="clave">Password:</label> 
+			<input type="password" name="password" id="clave"><br/>
+
+		</fieldset>
+		<input type="submit" value="Enviar">
+
+	</form>
+</body>
 </html>
